53eee594d324569e91da01e489556d780d74f10c
Chunchinlee <torresmichaellhance@gmail.com>
2024-10-03 17:12:12 +0800

    bnew

--- src/App.js
+++ src/App.js
@@ -6,44 +6,13 @@ import About from "./Components/About";
 import './Css/navbar.css';
 
 const App = () => {
-  const location = useLocation(); // Tracks current path
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <div>
-      {/* Show Navbar on all pages except login */}
-      {location.pathname !== '/login' && <Navbar onLoginClick={openModal} />}
-      
-      <Routes>
-        {/* Home route shows the welcome message */}
-        <Route path="/" element={(
           <section className='hero'>
             <h1>Welcome To Fixme</h1>
             <p>Learn and Play with our Interactive Game</p>
           </section>
-        )} />
-        {/* About route */}
-        <Route path="/about" element={<About />} />
-      </Routes>
-
-      {/* Login modal */}
-      <LoginModal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 };
-
-const AppWrapper = () => (
-  <Router>
-    <App />
-  </Router>
-);
-
-export default AppWrapper;
+export default App;
